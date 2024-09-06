439b547a3507d5ec0b4033e32bd766586ad7d81a
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-09-06 11:25:59 +0300

    Pokemon

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -29,7 +29,6 @@ class Pokedex extends Component {
             { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
             { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 },
 
-
         ],
     };
 
